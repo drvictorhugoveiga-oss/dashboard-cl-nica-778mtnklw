e042825f5c9c6a228e72140c3c3042bad17fa6f5
Skip <skip@usecurling.com>
2026-05-04 18:55:18 +0000

    feat: Implement professional and cost management

--- src/services/plans.ts
+++ src/services/plans.ts
@@ -5,3 +5,9 @@ export const getPlans = () => {
     sort: 'price',
   })
 }
+
+export const createPlan = (data: any) => pb.collection('plans').create(data)
+
+export const updatePlan = (id: string, data: any) => pb.collection('plans').update(id, data)
+
+export const deletePlan = (id: string) => pb.collection('plans').delete(id)
